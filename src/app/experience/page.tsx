fa965bad85b8d5de761301a39be5298b455dfd88
wsomad <87234180+haikalsamari@users.noreply.github.com>
2024-12-18 21:25:09 +0800

    Commit to gh-pages branch

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -3,7 +3,7 @@ const Experience: React.FC = () => {
     <div className='px-6 py-10 font-courier'>
         <h1 className="flex items-start justify-start text-3xl font-bold text-black">#MyExperience?</h1>
         <p className="mt-4 text-lg text-black">
-        This is the profile section where you can view my personal information and skills.
+        This is the profile on where you can view my personal information and skills.
         </p>
     </div>
     )
