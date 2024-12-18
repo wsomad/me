785ed5843a671ec1007082bb333882889601faec
wsomad <87234180+haikalsamari@users.noreply.github.com>
2024-12-18 22:14:15 +0800

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/me', // Update this to match your GitHub repo name or remove if it's a user/organization site.
+  assetPrefix: isProd ? '/me/' : '', // Same as above.
+  trailingSlash: true, // Ensure static paths are resolved properly.
 };
 
 export default nextConfig;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const AboutPage: React.FC = () => {
   return (
-    <div className='px-6 py-10 font-courier'>
+    <div className="px-6 py-10 font-courier">
       <h1 className="flex items-start justify-start text-3xl font-bold text-black">#WhoAmI?</h1>
       <p className="mt-4 text-lg text-black">
-        Hello, I'm <span className='font-bold'>Haikal</span>. I'm a passionate software developer with experience in various technologies.
+        Hello, I&apos;sm <span className='font-bold'>Haikal</span>. I&apos;sm a passionate software developer with experience in various technologies.
         This is a brief description of my journey and experiences.
       </p>
     </div>
