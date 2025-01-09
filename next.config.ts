14604183f23e38fca0129cfd581d4934e340c7ab
wsomad <87234180+haikalsamari@users.noreply.github.com>
2025-01-09 22:07:50 +0800

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const isProd: boolean = process.env.NODE_ENV === 'production';
 
 const nextConfig: import('next').NextConfig = {
-  //output: 'export',
+  output: 'export',
   basePath: isProd ? '/me' : '', // Use basePath only in production.
   assetPrefix: isProd ? '/me/' : '', // Use assetPrefix only in production.
   trailingSlash: true, // Ensures directories generate index.html files.
