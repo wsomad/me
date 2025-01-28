cfe02c513ede21d94e4625afd0bd5bafe8c7d3f0
wsomad <87234180+haikalsamari@users.noreply.github.com>
2025-01-28 21:25:42 +0800

    Fix education section

--- src/components/education/education.tsx
+++ src/components/education/education.tsx
@@ -2,9 +2,9 @@ import { education } from "@/types/experience";
 
 const Places: React.FC = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 pt-6">
             {education.map((e, index) => (
-                <div key={index} className="flex flex-row md:flex-col items-start md:items-center">
+                <div key={index} className="flex flex-row md:flex-col items-center justify-center md:items-center">
                     <div className="flex items-center justify-center w-10 h-10 bg-black text-white rounded-full font-semibold">
                         {index + 1}
                     </div>
