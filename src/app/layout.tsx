fedf26810b72534ba895c572eb7b98309d7f4aa1
wsomad <87234180+haikalsamari@users.noreply.github.com>
2025-01-30 21:31:18 +0800

    Add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,17 @@ interface LayoutProps {
   children: ReactNode;
 }
 
+export const metadata = {
+  title: "Muhammad Haikal | Online Portfolio",
+  description: "Portfolio of Muhammad Haikal, a software engineer specializing in web development and mobile development.",
+  keywords: "Muhammad Haikal, software engineer, online portfolio",
+  authors: [{ name: "Muhammad Haikal" }],
+  robots: "index, follow",
+  other: {
+    "google-site-verification": "dRZ0b2OXM3OA-M1RNZPld9UKmwRu50CBV6qELq1l2d0",
+  },
+};
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
