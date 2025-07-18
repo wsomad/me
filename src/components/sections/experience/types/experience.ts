c06c7ec0f1df542aa04b89b95ec3c2f9628f0bff
wsomad <87234180+haikalsamari@users.noreply.github.com>
2025-07-18 22:34:31 +0800

    Fix images not found issue

--- src/components/sections/experience/types/experience.ts
+++ src/components/sections/experience/types/experience.ts
@@ -6,6 +6,6 @@ export interface place {
 }
 
 export const experience: place[] = [
-    {name: "DC Technology Solutions  ", role: "Technology Analyst", duration: "Mac 2025 - Now", imagePath: `images/deloitte-logo.png`},
-    {name: "DC Technology Solutions  ", role: "Technology Intern", duration: "Sept 2024 - Jan 2025", imagePath: `images/deloitte-logo.png`},
+    {name: "DC Technology Solutions  ", role: "Technology Analyst", duration: "Mac 2025 - Now", imagePath: `/images/deloitte-logo.png`},
+    {name: "DC Technology Solutions  ", role: "Technology Intern", duration: "Sept 2024 - Jan 2025", imagePath: `/images/deloitte-logo.png`},
 ]
